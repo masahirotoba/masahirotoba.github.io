feca6b6b178ed01545af218324f7c48833f1ce4e
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2022-05-12 17:43:23 +0900

    Add files via upload

---
+++ web/get_method.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+    </head>
+        <body>
+        <h1>GETメソッドによるパラメータ渡し</h1>
+        <?php
+            $arg1 = $_GET['arg1'];
+            $arg2 = $_GET['arg2'];
+            $result = $arg1 + $arg2;
+            echo $result;
+        ?>
+        <p><a href = "../index.html">前に戻る</p>
+        </body>
+</html>
