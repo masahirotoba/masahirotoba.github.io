a70930f1a7fa70c2edb124b2126ad46bdbe91de7
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2022-05-13 23:26:22 +0900

    Update login_session.php

--- web/login/session/login_session.php
+++ web/login/session/login_session.php
@@ -1,9 +1,8 @@
 <?php
     $user = $_POST['user'];
     $pass = $_POST['pass'];
-    if(strcmp($pass, 'pass') == 0) {
+    if(strcmp($user,'user') == 0 && strcmp($pass, 'pass') == 0){
         session_start();
-        $_SESSION['user'] = $user;        
         header('Location: success_session.php');
     } else {
         header('Location: failed_session.html');
