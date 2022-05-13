5c71a64f85ea8016b13ec849602ee3ad23ea0dfc
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2022-05-13 09:50:07 +0900

    Update login_php.php

--- web/login_php.php
+++ web/login_php.php
@@ -1,18 +1,9 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transnational//EN">
-<html lang="ja">
-    <head>
-    </head>
-    <body>
-        <h1>PHPによるログイン認証</h1>
-        <?php
-            $user = $_POST['user'];
-            $pass = $_POST['pass'];
-            if(strcmp($user,'user') == 0 && strcmp($pass, 'pass') == 0{
-              header('Location: success.html');
-            } else {
-              header('Location: failed.html')+
-            }
-        ?>
-        <p><a href = "../index.html">前に戻る</p>
-    </body>
-</html>
+<?php
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
+    if(strcmp($user,'user') == 0 && strcmp($pass, 'pass') == 0{
+        header('Location: success.html');
+    } else {
+        header('Location: failed.html')+
+    }
+?>
