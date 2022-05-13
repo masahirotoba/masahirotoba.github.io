e59dad17fcb16e441b1d94f25201482171d274a8
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2022-05-13 09:59:42 +0900

    Update login_php.php

--- web/login_php.php
+++ web/login_php.php
@@ -4,6 +4,6 @@
     if(strcmp($user,'user') == 0 && strcmp($pass, 'pass') == 0){
         header('Location: success.html');
     } else {
-        header('Location: failed.html')+
+        header('Location: failed.html');
     }
 ?>
