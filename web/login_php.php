d8a454eccf45531a4d79afd0ca525a847b7d58af
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2022-05-13 11:25:56 +0900

    Delete login_php.php

--- web/login_php.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $user = $_POST['user'];
-    $pass = $_POST['pass'];
-    if(strcmp($user,'user') == 0 && strcmp($pass, 'pass') == 0){
-        header('Location: success.html');
-    } else {
-        header('Location: failed.html');
-    }
-?>
