089d79aa677887fdc29fcfa543b82458bccd4738
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2022-05-13 11:32:24 +0900

    Create success_cookie.php

---
+++ web/login/cookie/success_cookie.php
@@ -0,0 +1,24 @@
+<?php
+    header('Cache-Control: no-cache');
+    $user = $_COOKIE['user'];
+    $pass = $_COOKIE['pass'];
+    if(strcmp($user,'user') == 0 && strcmp($pass, 'pass') == 0){
+      setcookie("user","",time() -3600);
+      setcookie("pass","",time() -3600);      
+      header('Location: failed.html');
+      exit();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+    </head>
+    <body>
+      <h1>Cookieによるログイン認証</h1>
+      <h1>ログインに成功しました</h1>
+      <form action="do_logout.php">
+        <input type="submit" value="ログアウト" />
+      </form>
+    </body>
+</html>
